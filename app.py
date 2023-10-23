496c7961fadbe482f986e278af2c3bed285f16ea
Bryson Camp <brysoncamp152@gmail.com>
2023-10-23 16:21:33 +1000

    Debugging

--- app.py
+++ app.py
@@ -7,6 +7,7 @@ def index():
     return render_template('index.html')
 
 if __name__ == '__main__':
-    app.run(debug=True)
+    app.run(debug=True, host='0.0.0.0', port=8000)
+
 
 # pip freeze > requirements.txt
